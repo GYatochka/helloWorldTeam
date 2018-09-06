45414dac187bde7d3ab72d597dad7fb07a281eeb
Michael Benko <godyatochka@gmail.com>
2018-09-06 22:27:21 +0300

    add new classes

---
+++ Task1_Currency/Currency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Currency
+{
+    class Currency: IReadable
+    {
+        private string _currencyName;
+        private int _ammount;
+
+        public string CurrencyName {
+            get
+            {
+                return _currencyName;
+            }
+            set
+            {
+                _currencyName = value;
+            }
+        }
+        public int Ammount {
+            get {
+                return _ammount;
+            }
+            set {
+                if (value > 0)
+                {
+                    _ammount = value;
+                }
+                else
+                {
+                    throw new NotSupportedException("Wrong valeu!");
+                }
+            }
+        }
+
+        public void Read()
+        {
+            return;
+        }
+    }
+}
