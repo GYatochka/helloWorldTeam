f83cfc989181aacfa6e1a99121982bd376fb1582
Nadiyka52 <n.antoshuk52@gmail.com>
2018-09-19 21:59:26 +0300

    test1

---
+++ Task1_Currency.Tests/CurrencyTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1_Currency.Tests
+{
+    [TestClass]
+    public class CurrencyTest
+    {
+        [TestMethod]
+        public void Currencytest()
+        {
+            string currencyNameexp = "USD";
+            
+            float amountexp = 10;
+
+       
+            Currency c = new Currency(currencyNameexp, amountexp);
+            
+            Assert.AreEqual(amountexp, c.Amount);
+            Assert.AreEqual(currencyNameexp, c.CurrencyName);
+        }
+        [TestMethod]
+        public 
+    }
+
+}
+
+/*   
+        public Currency()
+        {
+            _amount = 0;
+            _currencyName = "UAH";
+        }
+        public Currency(string currencyName, float amount)
+        {
+            Amount = amount;
+            _currencyName = currencyName;
+        }
+
+}*/
