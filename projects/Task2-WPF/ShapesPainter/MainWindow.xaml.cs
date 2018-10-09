1f05ded532665cae6a4310253f51994ba7df5196
Michael Benko <godyatochka@gmail.com>
2018-10-10 00:15:09 +0300

    try to write menubar

--- projects/Task2-WPF/ShapesPainter/MainWindow.xaml.cs
+++ projects/Task2-WPF/ShapesPainter/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace ShapesPainter
 {
     /// <summary>
@@ -20,12 +21,50 @@ namespace ShapesPainter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _pictureName;
+
+        private MenuBar _menuBar;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+         private void Save_Click(object sender, RoutedEventArgs e)
+         {
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)cnv.RenderSize.Width,
+                 (int)cnv.RenderSize.Height, 96d, 96d, System.Windows.Media.PixelFormats.Default);
+             rtb.Render(cnv);
+
+             var crop = new CroppedBitmap(rtb, new Int32Rect(50, 50, 250, 250));
+
+             BitmapEncoder pngEncoder = new PngBitmapEncoder();
+             pngEncoder.Frames.Add(BitmapFrame.Create(crop));
+
+             using (var fs = System.IO.File.OpenWrite("logo.png"))
+             {
+                 pngEncoder.Save(fs);
+             }
         }
 
 
-
+         private void Create_Click(object sender, RoutedEventArgs e)
+         {
+             CreateWindow createWindow = new CreateWindow();
+             createWindow.Show();
+            // if (createWindow.isCreated)
+            // {
+                 _pictureName = createWindow.getName();
+                 createWindow.Close();
+             //}
+        }
     }
+
 }
+
+
---
+++ projects/Task2-WPF/ShapesPainter/MenuBar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace ShapesPainter
+{
+    class MenuBar
+    {
+        private string _pictureName;
+
+        public void open()
+        {
+            
+            
+        }
+
+        public void save(Canvas cnv)
+        {
+             /*RenderTargetBitmap rtb = new RenderTargetBitmap((int)cnv.RenderSize.Width,
+                (int)cnv.RenderSize.Height, 96d, 96d, System.Windows.Media.PixelFormats.Default);
+            rtb.Render(cnv);
+
+            var crop = new CroppedBitmap(rtb, new Int32Rect(50, 50, 250, 250));
+
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(crop));
+
+            using (var fs = System.IO.File.OpenWrite("logo.png"))
+            {
+                pngEncoder.Save(fs);
+            }
+
+           */
+           using (FileStream fs = new FileStream("MyPicture", FileMode.Create))
+             {
+                 RenderTargetBitmap bmp = new RenderTargetBitmap((int)cnv.ActualWidth,
+                     (int)cnv.ActualHeight, 1 / 96, 1 / 96, PixelFormats.Pbgra32);
+                 bmp.Render(cnv);
+                 BitmapEncoder encoder = new TiffBitmapEncoder();
+                 encoder.Frames.Add(BitmapFrame.Create(bmp));
+                 encoder.Save(fs);
+                 fs.Close();
+             }
+        }
+
+        public void create()
+        {
+            CreateWindow createWindow = new CreateWindow();
+            createWindow.Show();
+            if (createWindow.Created())
+            {
+                _pictureName = createWindow.getName();
+            }
+            
+        }
+    }
+}
