a3fb61643c5f48e395aa652d0e2dc485470c97bf
Michael Benko <godyatochka@gmail.com>
2018-10-29 17:32:02 +0200

    Add Product and Ticket classes
    Add realization of Product and Ticket classes with INotifyPropertyChanged realization inside

---
+++ projects/Task3(WPF)/Task3(WPF)/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Task3_WPF_
+{
+    /// <summary>
+    /// Model realization from MVVM pattern
+    /// </summary>
+    class Product : INotifyPropertyChanged
+    {
+        private string _name;
+        private float _price;
+
+        public string Name
+        {
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+            get { return _name; }
+        }
+        public float Price
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                    OnPropertyChanged("Price");
+                }
+                else
+                {
+                    throw new Exception("Invalid value!");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public override string ToString()
+        {
+            return "Name: "+ _name + " Price: " + _price;
+        }
+    }
+}
---
+++ projects/Task3(WPF)/Task3(WPF)/Ticket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Task3_WPF_
+{
+    /// <summary>
+    /// This class is viewmodel realization in our program.
+    /// </summary>
+    class Ticket : INotifyPropertyChanged
+    {
+        private Product _selectedProduct;
+
+        public ObservableCollection<Product> Sushies { get; set; }
+
+        public Product SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                OnPropertyChanged("SelectedProduct");
+            }
+        }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
