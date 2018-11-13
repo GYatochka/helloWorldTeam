b3df37a7a0b670449af0c6cc1780b7a65695a542
Bohdan Zahirnuy <zahirnuy@ukr.net>
2018-11-14 01:36:21 +0200

    added a project with Task4(for working you should download sql servel and managment and northwind database)

---
+++ projects/Task4(ADO.NET)/Task4(ADO.NET)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Task4_ADO.NET_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=.\SQLEXPRESS; Initial Catalog=NORTHWIND; Integrated Security=True";
+            SqlConnection connection = new SqlConnection(conStr);
+            try
+            {
+                connection.Open();
+                PrintInfo(connection);
+                
+              
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+
+            }
+            finally
+            {
+                connection.Close();
+                PrintInfo(connection);
+            }
+            Console.ReadKey();
+        }
+        static void PrintInfo(SqlConnection connection)
+        {
+            Console.WriteLine("Connection to: ");
+            Console.WriteLine("Data source: " + connection.DataSource);
+            Console.WriteLine("Data base: " + connection.Database);
+            Console.WriteLine("State: " + connection.State);
+
+        }
+    }
+}
